6a84adb50d27647b855d30886e8e52b21b260334
andriibeha <beha.andrii@gmail.com>
2022-12-16 19:08:16 +0200

    space

--- controllers/bloodDietProducts/getDiet.js
+++ controllers/bloodDietProducts/getDiet.js
@@ -5,10 +5,13 @@ const getDiet = async (req, res, next) => {
   //   "ФОРМУЛА ДЛЯ РОЗРАХУНКУ ДЕННОЇ НОРМИ КАЛОРІЙ ДЛЯ ЖІНОК
   // 10 * вага + 6.25 * зріст - 5 * вік - 161 - 10 * (вага - бажана вага)"
   const { blood, height, age, cWeight, dWeight } = req.body;
+
   const dailyCalorie = Math.round(
     10 * cWeight + 6.25 * height - 5 * age - 161 - 10 * (cWeight - dWeight)
   );
+
   const data = await BloodDietProduct.find();
+  
   const products = data
     .reduce((array, item) => {
       if (item.groupBloodNotAllowed[blood]) {
@@ -17,9 +20,11 @@ const getDiet = async (req, res, next) => {
       return array;
     }, [])
     .slice(0, 4);
+
   if (!products.length) {
     throw RequestError(404, 'Not found');
   }
+
   res.json({
     status: 'success',
     code: 200,
