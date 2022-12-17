53001a76e4d3a6659441ff7090ec0d8f6b364b8c
andriibeha <beha.andrii@gmail.com>
2022-12-17 15:22:35 +0200

    add swagger-ui-express and swagger.json

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const dailyproductsRouter = require('./routes/api/dailyproducts');
 const bloodDietProductsRouter = require('./routes/api/bloodDietProducts');
@@ -16,6 +18,9 @@ app.use(express.json());
 
 app.use('/api/dailyproducts', dailyproductsRouter);
 app.use('/api/bloodproducts', bloodDietProductsRouter);
+
+//Swager
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Not found' });
