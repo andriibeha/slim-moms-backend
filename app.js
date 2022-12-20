e13dc9fbe3d0658941df5374d57804d93b02e92b
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-12-20 08:26:12 +0200

    fix/dailyProducts

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
-app.use('/api/dailyproducts', dailyproductsRouter);
+app.use('/api/diary', dailyproductsRouter);
 app.use('/api/bloodproducts', bloodDietProductsRouter);
 
 //Swager
