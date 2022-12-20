cb84b43efb87462a3fb091f2cece645a4cf5b7f2
Andrey1914 <anlikajw@gmail.com>
2022-12-20 09:56:06 +0100

    added setPassword function

--- models/user.js
+++ models/user.js
@@ -39,6 +39,11 @@ const userSchema = Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
 userSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
