3dd989d6e3ca4608ee14796aa2e7003b67017995
Nadezhda Kovalenko <95223859+Nadina00@users.noreply.github.com>
2022-12-24 15:45:04 +0200

    remove category in search

--- controllers/bloodDietProductUser/bloodDietProductUser.js
+++ controllers/bloodDietProductUser/bloodDietProductUser.js
@@ -1,14 +1,14 @@
 const { BloodDietProduct } = require('../../models');
 
 const bloodDietProductsUser = async (req, res, next) => {
-  const {categories} = req.query
+ 
   const bloodType = req.user.bloodType;
   const { height, age, cWeight, dWeight } = req.body;
   const dailyCalorieUser = Math.round(
     10 * cWeight + 6.25 * height - 5 * age - 161 - 10 * (cWeight - dWeight)
   );
 
-  const result = await BloodDietProduct.find({categories});
+  const result = await BloodDietProduct.find({});
 
   const data = result.filter(
     result => result.groupBloodNotAllowed[bloodType] === true
